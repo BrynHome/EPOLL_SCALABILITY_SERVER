33f58dc8aa618f42cffac829ecbbafb9efd8cfc3
Bryan <b.r.hill@outlook.com>
2022-02-14 15:28:07 -0800

    WIP 1

--- Client/main.c
+++ Client/main.c
@@ -1,5 +1,7 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "openmp-use-default-none"
 /*
---  Compile: gcc -Wall -ggdb -o epollc epoll_clnt.c
+--  Compile: gcc -Wall -ggdb -o epollc epoll_clnt.c -fopenmp
 ---------------------------------------------------------------------------------------*/
 #include <stdio.h>
 #include <netdb.h>
@@ -13,11 +15,13 @@
 #include <unistd.h>
 #include <string.h>
 #include <pthread.h>
+#include <omp.h>
 
 #define SERVER_TCP_PORT		7000	// Default port
 #define BUFLEN			    1024 	// Buffer length
 #define CONNECTION_LIMIT    15000   // Max # of clients
 #define ITER_LIMIT          10000   // Max # of iterations
+long delay (struct timeval t1, struct timeval t2);
 pthread_cond_t cond1 = PTHREAD_COND_INITIALIZER;
 
 // declaring mutex
@@ -28,13 +32,13 @@ static void SystemFatal(const char* message)
     exit (EXIT_FAILURE);
 }
 
-int setup(int *num_connections, int *message_len, int *iterations)
+int setup(int *num_connections, int *message_len, int *iterations, int *connect_all)
 {
     char ibuf[BUFLEN];
     printf("Num clients(Limit here): ");
     fgets(ibuf, BUFLEN, stdin);
     *num_connections = atoi(ibuf);
-    if(1 > *num_connections||*num_connections > 15000)
+    if(1 > *num_connections||*num_connections > CONNECTION_LIMIT)
     {
         SystemFatal("Improper number of connections");
         return 0;
@@ -52,23 +56,36 @@ int setup(int *num_connections, int *message_len, int *iterations)
     printf("Times to send message(Limit here): ");
     fgets(ibuf, BUFLEN, stdin);
     *iterations = atoi(ibuf);
-    if(1 > *iterations||*iterations > 10000)
+    if(1 > *iterations||*iterations > ITER_LIMIT)
     {
         SystemFatal("Improper number of iterations");
         return 0;
     }
-    return 1;
+
+    printf("Wait for all clients to connect before sending?(1 for no, 2 for yes): ");
+    fgets(ibuf, BUFLEN, stdin);
+    *connect_all = atoi(ibuf);
+    if(1 == *connect_all || *connect_all == 2)
+    {
+        return 1;
+
+    } else{
+        SystemFatal("Improper set for wait for connection.");
+        return 0;
+    }
+
 }
 
 int main (int argc, char **argv)
 {
-    int n, bytes_to_read;
-    int sd, port;
-    int num_connections, message_len, iterations;
+
+    int port;
+    int num_connections, message_len, iterations, connect_all;
     struct hostent	*hp;
     struct sockaddr_in server;
-    char  *host, *bp, rbuf[BUFLEN], sbuf[BUFLEN],  **pptr;
+    char  *host, sbuf[BUFLEN];
     char str[16];
+    int thread_num=0,num_core =1;
 
 
     switch(argc)
@@ -86,7 +103,7 @@ int main (int argc, char **argv)
             exit(1);
     }
 
-    if(!setup(&num_connections,&message_len,&iterations))
+    if(!setup(&num_connections,&message_len,&iterations,&connect_all))
     {
         SystemFatal("Setup issue");
     }
@@ -95,18 +112,7 @@ int main (int argc, char **argv)
     memset(sbuf,'X',message_len);
     sbuf[message_len] = '\0';
     int all_conn=0;
-
-    //fork or create threads here
-
-    //NEED TO ADD VARIABLE TO CONTROL IF ALL CONNECT BEFORE SENDING
-    pthread_mutex_lock(&lock);
-    // Create the socket
-    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-    {
-        perror("Cannot create socket");
-        exit(1);
-    }
-
+    omp_set_num_threads(num_connections);
     bzero((char *)&server, sizeof(struct sockaddr_in));
     server.sin_family = AF_INET;
     server.sin_port = htons(port);
@@ -117,51 +123,86 @@ int main (int argc, char **argv)
         exit(1);
     }
     bcopy(hp->h_addr, (char *)&server.sin_addr, hp->h_length);
-
-    // Connecting to the server
-    if (connect (sd, (struct sockaddr *)&server, sizeof(server)) == -1)
+    //fork or create threads here
+    #pragma omp parallel private(thread_num,num_core)
     {
-        fprintf(stderr, "Can't connect to server\n");
-        perror("connect");
-        exit(1);
-    }
-
-    printf("Connected:    Server Name: %s\n", hp->h_name);
-    pptr = hp->h_addr_list;
-    printf("\t\tIP Address: %s\n", inet_ntop(hp->h_addrtype, *pptr, str, sizeof(str)));
-    if(all_conn < num_connections) {
-        all_conn++;
-        pthread_cond_wait(&cond1,&lock);
-    } else {
-        printf("all connected");
-        pthread_cond_signal(&cond1);
-    }
-    pthread_mutex_unlock(&lock);
-    printf("Transmit:\n");
-
-    // get user's text
-    for(int b = 0; b < iterations;b++) {
-        send (sd, sbuf, BUFLEN, 0);
-
-        printf("Receive:\n");
-        bp = rbuf;
-        bytes_to_read = BUFLEN;
-
-        // client makes repeated calls to recv until no more data is expected to arrive.
-        n = 0;
-        while ((n = recv (sd, bp, bytes_to_read, 0)) < BUFLEN)
-        {
-            bp += n;
-            bytes_to_read -= n;
+        int n, bytes_to_read,sd;
+        char *bp, rbuf[BUFLEN],  **pptr;
+        num_core = omp_get_num_threads();
+        thread_num = omp_get_thread_num();
+        //NEED TO ADD VARIABLE TO CONTROL IF ALL CONNECT BEFORE SENDING
+        if(connect_all == 2) {
+            pthread_mutex_lock(&lock);
         }
-        printf ("%s\n", rbuf);
-        fflush(stdout);
+        // Create the socket
+        if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+        {
+            perror("Cannot create socket");
+            exit(1);
+        }
+
+
+
+        // Connecting to the server
+        if (connect (sd, (struct sockaddr *)&server, sizeof(server)) == -1)
+        {
+            fprintf(stderr, "Can't connect to server\n");
+            perror("connect");
+            exit(1);
+        }
+
+        printf("Connected:    Server Name: %s\n", hp->h_name);
+        pptr = hp->h_addr_list;
+        printf("\t\tIP Address: %s\n", inet_ntop(hp->h_addrtype, *pptr, str, sizeof(str)));
+        if(connect_all == 2) {
+            if(all_conn < num_connections) {
+                all_conn++;
+                pthread_cond_wait(&cond1,&lock);
+            } else {
+                printf("all connected");
+                pthread_cond_signal(&cond1);
+            }
+            pthread_mutex_unlock(&lock);
+        }
+
+        //printf("Transmit:\n");
+
+        // get user's text
+        for(int b = 0; b < iterations;b++) {
+            send (sd, sbuf, BUFLEN, 0);
+
+            //printf("Receive:\n");
+            bp = rbuf;
+            bytes_to_read = BUFLEN;
+
+            // client makes repeated calls to recv until no more data is expected to arrive.
+            n = 0;
+            while ((n = recv (sd, bp, bytes_to_read, 0)) < BUFLEN)
+            {
+                bp += n;
+                bytes_to_read -= n;
+            }
+            printf ("%s\n", rbuf);
+            fflush(stdout);
+        }
+        //fgets (sbuf, BUFLEN, stdin);
+
+        // Transmit data through the socket
+
+        close (sd);
     }
-    //fgets (sbuf, BUFLEN, stdin);
 
-    // Transmit data through the socket
-
-    close (sd);
     return (0);
 }
 
+long delay (struct timeval t1, struct timeval t2)
+{
+    long d;
+
+    d = (t2.tv_sec - t1.tv_sec) * 1000;
+    d += ((t2.tv_usec - t1.tv_usec + 500) / 1000);
+    return(d);
+}
+
+
+#pragma clang diagnostic pop
